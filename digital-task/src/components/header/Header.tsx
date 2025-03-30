8f11a3483a56083b74e3130941c70f78c002138c
TeaChareqishvili <teachareqishvili89@gmail.com>
2025-03-30 22:35:43 +0400

    header

---
+++ digital-task/src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./Logo";
+import HorizontalMenu from "./HorizontalMenu";
+
+export default function Header() {
+  return (
+    <>
+      <Logo />
+      <HorizontalMenu />
+    </>
+  );
+}
---
+++ digital-task/src/components/header/HeaderLayout.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from "react";
+
+type HeaderLayoutProps = {
+  children: ReactNode;
+};
+
+export default function HeaderLayout({ children }: HeaderLayoutProps) {
+  return <div className="header-container">{children}</div>;
+}

2df56260f0c2fedf946800f30cccbab2ad427c77
TeaChareqishvili <teachareqishvili89@gmail.com>
2025-03-31 21:03:14 +0400

    header sticky position

--- digital-task/src/components/header/HorizontalMenu.tsx
+++ digital-task/src/components/header/HorizontalMenu.tsx
@@ -1,9 +1,32 @@
 import menu from "../../data";
 import arrowRight from "../../assets/arrowRight.png";
+import { useEffect, useState, useCallback } from "react";
 
 export default function HorizontalMenu() {
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = useCallback(() => {
+    const currentScrollPos = window.scrollY;
+    const isScrollingDown = currentScrollPos > lastScrollTop;
+    console.log(currentScrollPos, "scrol");
+
+    if (isScrollingDown && currentScrollPos > 200) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+
+    setLastScrollTop(currentScrollPos);
+  }, [lastScrollTop]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
   return (
-    <div className="logo">
+    <div className={`navigation ${visible ? "" : "hide-menu"}`}>
       <ul className="menu-bar">
         {menu.map((item) => (
           <li key={item.id} className="menu">

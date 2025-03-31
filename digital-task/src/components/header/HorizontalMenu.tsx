29e540a7e6567b0a62e28471b27ad09586a4f4e5
TeaChareqishvili <teachareqishvili89@gmail.com>
2025-03-31 11:33:17 +0400

    add drop down menu

--- digital-task/src/components/header/HorizontalMenu.tsx
+++ digital-task/src/components/header/HorizontalMenu.tsx
@@ -1,4 +1,5 @@
 import menu from "../../data";
+import arrowRight from "../../assets/arrowRight.png";
 
 export default function HorizontalMenu() {
   return (
@@ -8,6 +9,15 @@ export default function HorizontalMenu() {
           <li key={item.id} className="menu">
             {item.text}
             {item.icon && <img src={item.icon} alt="icon" />}
+            {item.dropdown && (
+              <ul className="dropdown">
+                {item.dropdown.map((subitem) => (
+                  <li key={subitem.id}>
+                    {subitem.text} <img src={arrowRight} alt="icon" />
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
       </ul>
--- digital-task/src/data.ts
+++ digital-task/src/data.ts
@@ -4,6 +4,7 @@ export type MenuItem = {
   id: number;
   text: string;
   icon?: string;
+  dropdown?: MenuItem[];
 };
 
 const menu: MenuItem[] = [
@@ -11,26 +12,54 @@ const menu: MenuItem[] = [
     id: 1,
     text: "Demos",
     icon: icon,
+    dropdown: [
+      { id: 1, text: "Demo" },
+      { id: 2, text: "Live Demo" },
+    ],
   },
   {
     id: 2,
     text: "Post",
     icon: icon,
+    dropdown: [
+      { id: 1, text: "Health" },
+      { id: 2, text: "Gym" },
+      { id: 3, text: "Sports" },
+      { id: 4, text: "News" },
+    ],
   },
   {
     id: 3,
     text: "Features",
     icon: icon,
+    dropdown: [
+      { id: 1, text: "Post Header" },
+      { id: 2, text: "Post Layout" },
+      { id: 3, text: "Share buttons" },
+      { id: 4, text: "Gallery Post" },
+      { id: 5, text: "Video Post" },
+    ],
   },
   {
     id: 4,
     text: "Categories",
     icon: icon,
+    dropdown: [
+      { id: 1, text: "Health" },
+      { id: 2, text: "Gym" },
+      { id: 3, text: "Sports" },
+      { id: 4, text: "News" },
+    ],
   },
   {
     id: 5,
     text: "Shop",
     icon: icon,
+    dropdown: [
+      { id: 1, text: "Your Card" },
+      { id: 2, text: "Payment History" },
+      { id: 3, text: "Gift Cards " },
+    ],
   },
   {
     id: 6,
